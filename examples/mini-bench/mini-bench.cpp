8028a04e01240059e7eb2e0be44df2f8cba74056
Szilard Ledan <szledan@gmail.com>
2016-06-23 23:10:05 +0200

    Rework the implementation
    Note: Found an interesting behaviour.
    
    Signed-off-by: Szilard Ledan <szledan@gmail.com>

--- examples/mini-bench/mini-bench.cpp
+++ examples/mini-bench/mini-bench.cpp
@@ -27,101 +27,216 @@
 #include "gepard-xsurface.h"
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <map>
 #include <math.h>
-#include <string>
+#include <memory>
 #include <sstream>
+#include <string>
 #include <time.h>
 
+namespace {
+
+#ifdef LOG
+#undef LOG
+#endif
+#define LOG(X) do { std::cout << X << std::endl; } while(false)
+
+class MiniBench;
+typedef std::unique_ptr<MiniBench> MiniBenchUniPtr;
 typedef std::map<std::string, int> ConfigMap;
-ConfigMap s_configs;
 
-inline void step()
+template<typename T>
+T clamp(T x, const T min, const T max)
 {
-    static time_t currentTime, oldTime = 0;
-    static int steps = 0;
+    return std::min(std::max(x, min), max);
+}
 
-    steps++;
-    time(&currentTime);
+class MiniBench {
+public:
+    MiniBench(ConfigMap& configMap)
+        : configs(configMap)
+        , sum(0)
+        , currentTime(0)
+        , oldTime(0)
+        , steps(0)
+    {
+        warmupCount = -configs["warmupCount"];
+    }
 
-    if (currentTime - oldTime >= 1) {
-        static int warmupCount = -s_configs["warmupCount"];
-        static long long unsigned int sum = 0;
+    void step()
+    {
+        steps++;
+        time(&currentTime);
 
-        if (warmupCount >= 0) {
-            sum += steps;
+        if (currentTime - oldTime >= 1) {
+
+            if (warmupCount >= 0) {
+                sum += steps;
+            }
+            warmupCount++;
+
+            float avg = 0;
+            if (warmupCount > 0) {
+                avg = float(sum) / float(warmupCount);
+            }
+            LOG(warmupCount << ". FPS: " << steps << ", AVG: " << avg << "");
+
+            // Reset.
+            steps = 0;
+            time(&oldTime);
         }
-        warmupCount++;
+    }
 
-        float avg = 0;
-        if (warmupCount > 0) {
-            avg = float(sum) / float(warmupCount);
+    virtual void start() = 0;
+    virtual void run() = 0;
+    virtual void stop() = 0;
+
+    ConfigMap& configs;
+    long long unsigned int sum;
+    time_t currentTime;
+    time_t oldTime ;
+    int steps;
+    int warmupCount;
+};
+
+class SnakeBench : public MiniBench {
+public:
+    SnakeBench(ConfigMap configMap)
+        : MiniBench(configMap)
+        , windowWidth(configs["windowWidth"])
+        , windowHeight(configs["windowHeight"])
+        , maxVelocity(configs["maxVelocity"])
+        , paintRectSize(configs["paintRectSize"])
+        , rectNumbers(configs["rectNumbers"])
+        , rectWidth(configs["rectWidth"])
+        , rectHeight(configs["rectHeight"])
+        , maxSize2(2 * paintRectSize)
+        , maxFlet(2 * paintRectSize * paintRectSize)
+        , ratio(float(255) / float(maxFlet))
+        , iterateCount(configs["iterateCount"])
+    {
+        std::srand(configs["seed"]);
+
+        std::unique_ptr<gepard::XSurface> s(new gepard::XSurface(windowWidth, windowHeight));
+        surface = std::move(s);
+        std::unique_ptr<gepard::Gepard> g(new gepard::Gepard(surface.get()));
+        gepard = std::move(g);
+    }
+
+    void drawRects(int x, int y)
+    {
+        for (int i = 0; i < rectNumbers; ++i) {
+            int dx = std::rand() % (maxSize2) - paintRectSize;
+            int dy = std::rand() % (maxSize2) - paintRectSize;
+            int a = 255 - int(float(dx * dx + dy * dy) * ratio);
+            gepard->setFillColor(std::rand() % 256, std::rand() % 256, std::rand() % 256, a);
+            gepard->fillRect(x + dx, y + dy, rectWidth, rectHeight);
+            step();
+         }
+    }
+
+    int newVelocity(int x)
+    {
+        const int sign = x < 0 ? -1 : 1;
+
+        x = abs(x + std::rand() % 7 - 3);
+
+        if (x < 1) {
+            x = 1;
         }
-        std::cout << warmupCount << ". FPS: " << steps << ", AVG: " << avg << "" << std::endl;
 
-        // Reset.
-        steps = 0;
-        time(&oldTime);
-    }
-}
+        x = clamp(x, 1, maxVelocity);
 
-void drawRects(gepard::Gepard& g, int x, int y)
-{
-    static const int paintRectSize = s_configs["paintRectSize"];
-    static const int rectNumbers = s_configs["rectNumbers"];
-    static const int rectWidth = s_configs["rectWidth"];
-    static const int rectHeight = s_configs["rectHeight"];
-
-    static const int s_maxSize2 = 2 * paintRectSize;
-    static const int s_maxFlet = 2 * paintRectSize * paintRectSize;
-    static const float s_ratio = float(255) / float(s_maxFlet);
-
-    for (int i = 0; i < rectNumbers; ++i) {
-        int dx = std::rand() % (s_maxSize2) - paintRectSize;
-        int dy = std::rand() % (s_maxSize2) - paintRectSize;
-        int a = 255 - int(float(dx * dx + dy * dy) * s_ratio);
-        g.setFillColor(std::rand() % 256, std::rand() % 256, std::rand() % 256, a);
-        g.fillRect(x + dx, y + dy, rectWidth, rectHeight);
-        step();
-     }
-}
-
-inline int newVelocity(int x)
-{
-    const int sign = x < 0 ? -1 : 1;
-    static const int maxVelocity = s_configs["maxVelocity"];
-
-    x = abs(x + std::rand() % 7 - 3);
-
-    if (x < 1) {
-        x = 1;
-    }
-    if (x > maxVelocity) {
-        x = maxVelocity;
+        return sign * x;
     }
 
-    return sign * x;
-}
+    void start()
+    {
+        LOG("SnakeBenchMark start...");
+    }
+
+    void stop()
+    {
+        LOG("...SnakeBenchMark stop.");
+    }
+
+    void run()
+    {
+        int x = (windowWidth / 4) + std::rand() % (windowWidth / 2);
+        int y = (windowHeight / 4) + std::rand() % (windowHeight / 2);
+        int dx = std::rand() % maxVelocity - trunc(maxVelocity / 2);
+        int dy = std::rand() % maxVelocity - trunc(maxVelocity / 2);
+
+        XEvent e;
+
+        for (int i = 0; i <= iterateCount; ++i) {
+            x += dx;
+            if (x - paintRectSize < 0 || x + paintRectSize > windowWidth) {
+                dx = -dx;
+                x += dx;
+                dx = newVelocity(dx);
+            }
+
+            y += dy;
+            if (y - paintRectSize < 0 || y + paintRectSize > windowHeight) {
+                dy = -dy;
+                y += dy;
+                dy = newVelocity(dy);
+            }
+
+            drawRects(x, y);
+
+            if (XCheckWindowEvent((Display*)surface->getDisplay(), (Window)surface->getWindow(), KeyPress | ClientMessage, &e)) {
+                break;
+            }
+        }
+    }
+
+    const int windowWidth;
+    const int windowHeight;
+    const int maxVelocity;
+    const int paintRectSize;
+    const int rectNumbers;
+    const int rectWidth;
+    const int rectHeight;
+    const int maxSize2;
+    const int maxFlet;
+    const float ratio;
+    const int iterateCount;
+
+    std::unique_ptr<gepard::XSurface> surface;
+    std::unique_ptr<gepard::Gepard> gepard;
+};
+
+} // anonymous namespace
 
 int main(int argc, char* argv[])
 {
-    std::cout << "*** Mini-BenchMark for Gepard, 2016, *** " << std::endl;
-    std::cout << "Usage: " << argv[0] << " [config-file]" << std::endl;
+    LOG("*** Mini-BenchMark for Gepard, 2016, *** ");
+    LOG("Usage: " << argv[0] << " [config-file]");
+
+    std::string configFile = "";
+    if (argc > 1) {
+        configFile = argv[1];
+    }
 
     // Set default configs.
-    s_configs["paintRectSize"] = 30;
-    s_configs["rectHeight"] = 5;
-    s_configs["rectNumbers"] = 20;
-    s_configs["rectWidth"] = 5;
-    s_configs["seed"] = 1985;
-    s_configs["maxVelocity"] = 51;
-    s_configs["warmupCount"] = 3;
-    s_configs["windowHeight"] = 500;
-    s_configs["windowWidth"] = 500;
+    ConfigMap configs;
+    configs["maxVelocity"] = 51;
+    configs["paintRectSize"] = 30;
+    configs["rectHeight"] = 5;
+    configs["rectNumbers"] = 20;
+    configs["rectWidth"] = 5;
+    configs["seed"] = 1985;
+    configs["warmupCount"] = 3;
+    configs["windowHeight"] = 500;
+    configs["windowWidth"] = 500;
+    configs["iterateCount"] = 1000;
 
-    std::cout << "Read config file...";
-    if (argc > 1) {
-        std::fstream fs(argv[1], std::fstream::in);
+    LOG("Read config file...");
+    if (configFile.empty()) {
+        std::fstream fs(configFile, std::fstream::in);
         std::string line;
 
         while (std::getline(fs, line)) {
@@ -132,64 +247,42 @@ int main(int argc, char* argv[])
                 std::string value;
 
                 if (std::getline(issLine, value)) {
-                    s_configs[key] = atoi(value.c_str());
+                    configs[key] = atoi(value.c_str());
                 }
             }
         }
 
-        std::cout << "done.";
+        LOG("...done.");
     } else {
-        std::cout << "not found. Use default configs.";
+        LOG("...not found. Use default configs.");
     }
-    std::cout << std::endl << std::endl;
+    LOG("");
 
-    std::cout << " * Configs:" << std::endl;
-    for (ConfigMap::iterator config = s_configs.begin(); config != s_configs.end(); ++config) {
-        std::cout << config->first << "=" << config->second << std::endl;
+    LOG(" * Configs:");
+    for (ConfigMap::iterator config = configs.begin(); config != configs.end(); ++config) {
+        LOG(config->first << "=" << config->second);
     }
-    std::cout << std::endl;
+    LOG("");
 
-    std::cout << "Mini-BenchMark start..." << std::endl;
+    std::list<MiniBenchUniPtr> benchMarks;
 
-    static const int windowWidth = s_configs["windowWidth"];
-    static const int windowHeight = s_configs["windowHeight"];
-    static const int maxVelocity = s_configs["maxVelocity"];
-    static const int paintRectSize = s_configs["paintRectSize"];
+    MiniBenchUniPtr bm1(new SnakeBench(configs));
+    benchMarks.push_back(std::move(bm1));
 
-    std::srand(s_configs["seed"]);
+    MiniBenchUniPtr bm2(new SnakeBench(configs));
+    benchMarks.push_back(std::move(bm2));
 
-    int x = (windowWidth / 4) + std::rand() % (windowWidth / 2);
-    int y = (windowHeight / 4) + std::rand() % (windowHeight / 2);
-    int dx = std::rand() % maxVelocity - trunc(maxVelocity / 2);
-    int dy = std::rand() % maxVelocity - trunc(maxVelocity / 2);
+    MiniBenchUniPtr bm3(new SnakeBench(configs));
+    benchMarks.push_back(std::move(bm3));
 
-    // Draw on XWindow
-    gepard::XSurface surface(windowWidth, windowHeight);
-    gepard::Gepard gepard(&surface);
-    XEvent e;
 
-    while (true) {
-        x += dx;
-        if (x - paintRectSize < 0 || x + paintRectSize > windowWidth) {
-            dx = -dx;
-            x += dx;
-            dx = newVelocity(dx);
-        }
-
-        y += dy;
-        if (y - paintRectSize < 0 || y + paintRectSize > windowHeight) {
-            dy = -dy;
-            y += dy;
-            dy = newVelocity(dy);
-        }
-
-        drawRects(gepard, x, y);
-
-        if (XCheckWindowEvent((Display*)surface.getDisplay(), (Window)surface.getWindow(), KeyPress | ClientMessage, &e)) {
-            break;
-        }
+    LOG("Mini-BenchMark start...");
+    for (std::list<MiniBenchUniPtr>::iterator it = benchMarks.begin(); it != benchMarks.end(); ++it) {
+        it->get()->start();
+        it->get()->run();
+        it->get()->stop();
     }
+    LOG("Stop");
 
-    std::cout << "Stop" << std::endl;
     return 0;
 }
