3f5511d1dfe32a7ddcf20159a8e0ea08234e5110
Kristof Kosztyo <kkristof@inf.u-szeged.hu>
2018-01-18 19:05:47 +0100

    Push the X11-xcb dependency to the Vulkan port
    Signed-off-by: Kristof Kosztyo <kkristof@inf.u-szeged.hu>

--- cmake/deps/FindVULKAN.cmake
+++ cmake/deps/FindVULKAN.cmake
@@ -6,3 +6,6 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(VULKAN
                                   REQUIRED_VARS VULKAN_INCLUDE_DIR)
 
 include_directories(${VULKAN_INCLUDE_DIR})
+
+# TODO: Find a way to remove this dependency of the vulkan port
+list(APPEND LIB_DEPENDENCIES X11-xcb)
