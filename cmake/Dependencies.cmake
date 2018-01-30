81a36d7f6f89bb9a10b9c7adb2126df877e64b68
Dániel Bátyai <dbatyai@inf.u-szeged.hu>
2018-01-30 23:10:08 +0100

    Move vulkan_headers target to dependencies
    Signed-off-by: Dániel Bátyai <dbatyai@inf.u-szeged.hu>

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -9,7 +9,24 @@ if (BACKEND STREQUAL "GLES2")
   list(APPEND GEPARD_DEP_INCLUDES ${GLESv2_INCLUDE_DIR} ${EGL_INCLUDE_DIR})
 elseif (BACKEND STREQUAL "VULKAN")
   find_package(Vulkan)
-  if (VULKAN_FOUND)
-    list(APPEND GEPARD_DEP_INCLUDES ${VULKAN_INCLUDE_DIR})
+  if (NOT VULKAN_FOUND)
+    message(STATUS "Vulkan headers will be downloaded in a build step")
+    add_custom_command (OUTPUT ${PROJECT_BINARY_DIR}/thirdparty/include/vulkan/vulkan.h
+                        COMMAND mkdir -p "${PROJECT_BINARY_DIR}/thirdparty/include/vulkan"
+                        COMMAND wget -O "${PROJECT_BINARY_DIR}/thirdparty/include/vulkan/vulkan.h"
+                                        "https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/1.0/src/vulkan/vulkan.h")
+
+    add_custom_command (OUTPUT ${PROJECT_BINARY_DIR}/thirdparty/include/vulkan/vk_platform.h
+                        COMMAND mkdir -p "${PROJECT_BINARY_DIR}/thirdparty/include/vulkan"
+                        COMMAND wget -O "${PROJECT_BINARY_DIR}/thirdparty/include/vulkan/vk_platform.h"
+                                        "https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/1.0/src/vulkan/vk_platform.h")
+
+    add_custom_target(vulkan_headers
+                      DEPENDS ${PROJECT_BINARY_DIR}/thirdparty/include/vulkan/vulkan.h
+                              ${PROJECT_BINARY_DIR}/thirdparty/include/vulkan/vk_platform.h)
+
+    set(VULKAN_INCLUDE_DIR ${PROJECT_BINARY_DIR}/thirdparty/include)
   endif()
+
+  list(APPEND GEPARD_DEP_INCLUDES ${VULKAN_INCLUDE_DIR})
 endif ()
