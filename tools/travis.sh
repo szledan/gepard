ccce91d4ecfc57f3a39e7a2caa0b6ffaed9ecff5
Dániel Bátyai <dbatyai@inf.u-szeged.hu>
2018-02-02 14:22:41 +0100

    Update copyright
    Signed-off-by: Dániel Bátyai <dbatyai@inf.u-szeged.hu>

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Copyright (C) 2016, Gepard Graphics
-# Copyright (C) 2016, Dániel Bátyai <dbatyai@inf.u-szeged.hu>
+# Copyright (C) 2016, 2018 Gepard Graphics
+# Copyright (C) 2016, 2018 Dániel Bátyai <dbatyai@inf.u-szeged.hu>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
