32df5569a4e5083a8e873049adb9436f08a9762c
Dániel Bátyai <dbatyai@inf.u-szeged.hu>
2018-10-02 21:22:39 +0200

    Rework thirdparty dependencies
    This patch separates third party libraries from the main build system.
    The thirdparty directory recieved it's own CMakeLists.txt, which can be
    used to build required dependencies for each backend. Dependencies are
    stamped on a per-backend basis, which means they will only be built
    once, and will not be rebuilt later, unless explicitly specified.

--- tools/build.py
+++ tools/build.py
@@ -26,6 +26,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import argparse
+import dependencies
 import subprocess
 import sys
 import util
@@ -62,6 +63,7 @@ def add_args(parser):
     parser.add_argument('--build-dir', '-b', action='store', dest='build_dir', help='Specify build directory.')
     parser.add_argument('--install-prefix', action='store', dest='install_prefix', help='Specify install prefix.')
     parser.add_argument('--debug', '-d', action='store_const', const='debug', default='release', dest='build_type', help='Build debug.')
+    parser.add_argument('--rebuild-deps', action='store_true', default=False, help='Rebuild thirdparty dependencies.')
     parser.add_argument('--backend', action='store', choices=['gles2', 'software', 'vulkan'], default='gles2', help='Specify which graphics back-end to use.')
     parser.add_argument('--log-level', '-l', action='store', type=int, choices=range(0,5), default=0, help='Set logging level.')
     parser.add_argument('--no-colored-logs', action='store_true', default=False, help='Disable colored log messages.')
@@ -76,22 +78,20 @@ def get_args(skip_unknown=False):
     return parser.parse_args()
 
 
-def configure(arguments):
+def configure(source_path, build_path, arguments=None):
     """ Configures the build based on the supplied arguments. """
-    build_path = util.get_build_path(arguments)
-
     try:
         makedirs(build_path)
     except OSError:
         pass
 
-    util.call(['cmake', '-B' + build_path, '-H' + util.get_base_path()] + create_options(arguments))
+    util.call(['cmake', '-B' + build_path, '-H' + source_path] + create_options(arguments))
 
 
-def build_targets(arguments):
-    """ Runs the build. """
-    build_command = ['make', '-s', '-C', util.get_build_path(arguments)]
-    build_command.extend(arguments.targets)
+def build_targets(build_path, targets):
+    """ Builds the specified targets. """
+    build_command = ['make', '-s', '-C', build_path]
+    build_command.extend(targets)
 
     util.call(build_command)
 
@@ -108,8 +108,10 @@ def main():
     arguments = get_args()
 
     try:
-        configure(arguments)
-        build_targets(arguments)
+        dependencies.build_dependencies([arguments.backend], arguments.rebuild_deps)
+        build_path = util.get_build_path(arguments)
+        configure(util.get_base_path(), build_path, arguments)
+        build_targets(build_path, arguments.targets)
     except util.CommandError as e:
         print("Build failed: %s" % e)
         sys.exit(e.code)
