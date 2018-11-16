2d350d58e6f2b9ebd5a394e2172cda7092301ed2
Szilard Ledan <szledan@gmail.com>
2018-11-16 10:58:18 +0100

    [gles2] Change setting of antialias level
    Signed-off-by: Szilard Ledan <szledan@gmail.com>

--- cmake/OptionList.cmake
+++ cmake/OptionList.cmake
@@ -27,4 +27,5 @@ include(OptionMacros)
 
 ADD_CHOICE (BACKEND "Backend to use" "GLES2 SOFTWARE VULKAN" GLES2)
 ADD_OPTION (LOG_LEVEL "Print log messages during execution" 0)
+ADD_OPTION (ANTIALIAS_LEVEL "Set antialiassing level" 16)
 ADD_OPTION (DISABLE_LOG_COLORS "Do not color log messages" OFF)
