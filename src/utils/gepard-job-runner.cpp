888762fbf64cc105bf24eb09b09c855a5d95e6ec
Szilard Ledan <szledan@gmail.com>
2019-01-15 01:36:30 +0100

    Add JobRunner
    Signed-off-by: Szilard Ledan <szledan@gmail.com>

---
+++ src/utils/gepard-job-runner.cpp
@@ -0,0 +1,115 @@
+/* Copyright (C) 2019, Gepard Graphics
+ * Copyright (C) 2019, Szilard Ledan <szledan@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "gepard-job-runner.h"
+
+#include "gepard-defs.h"
+#include <chrono>
+#include <iostream>
+#include <string>
+
+namespace gepard {
+
+JobRunner::Job::Job(std::function<void ()> func, std::function<void ()> callback)
+    : boundFunc(func)
+    , callbackFunc(callback)
+{
+}
+
+void JobRunner::Job::run()
+{
+    GD_ASSERT(boundFunc != nullptr);
+    boundFunc();
+    if (callbackFunc != nullptr) {
+        callbackFunc();
+    }
+}
+
+JobRunner::JobRunner(const unsigned int workerCount)
+{
+    unsigned int wc = (workerCount && workerCount < 8) ? workerCount : 1;
+    do {
+        _workers.push_back(std::thread(worker, this));
+    } while (--wc);
+}
+
+JobRunner::~JobRunner()
+{
+    while (!finish) {
+        { // lock
+            std::lock_guard<std::mutex> guard(queueMutex);
+            if (queue.empty() && !activeJobCount) {
+                finish = true;
+            }
+        } // unlock
+        condVar.notify_all();
+        std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    };
+
+    for (size_t i = 0; i < _workers.size(); ++i) {
+        _workers[i].join();
+    }
+}
+
+void JobRunner::addJob(std::function<void()> func, std::function<void()> callback)
+{
+    { // lock
+        std::lock_guard<std::mutex> guard(queueMutex);
+        queue.push(new Job(func, callback));
+    } // unlock
+    condVar.notify_one();
+}
+
+void JobRunner::worker(JobRunner* jobScheduler)
+{
+    while (true) {
+        Job* job;
+        { // lock
+            std::unique_lock<std::mutex> lock(jobScheduler->queueMutex);
+            if (jobScheduler->queue.empty()) {
+                jobScheduler->condVar.wait(lock, [&]{ return jobScheduler->finish || !jobScheduler->queue.empty(); });
+            }
+            if (jobScheduler->finish) {
+                break;
+            }
+            if (jobScheduler->queue.empty()) {
+                continue;
+            }
+            job = jobScheduler->queue.front();
+            jobScheduler->queue.pop();
+            jobScheduler->activeJobCount++;
+        } // unlock
+
+        job->run();
+        delete job;
+
+        { // lock
+            std::lock_guard<std::mutex> guard(jobScheduler->queueMutex);
+            jobScheduler->activeJobCount--;
+        } // unlock
+    };
+}
+
+} // namespace gepard
---
+++ tests/unit/gepard-job-runner-tests.h
@@ -0,0 +1,112 @@
+/* Copyright (C) 2019, Gepard Graphics
+ * Copyright (C) 2019, Szilard Ledan <szledan@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GEPARD_JOB_SCHEDULER_TESTS_H
+#define GEPARD_JOB_SCHEDULER_TESTS_H
+
+#include "gepard-job-runner.h"
+#include "gtest/gtest.h"
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+namespace {
+
+struct TestClass {
+    void printThreadIDAndWait()
+    {
+        std::cerr << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    void incNumberAndWait(const int64_t milliseconds)
+    {
+        number++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+    }
+
+    void addValue(const int value)
+    {
+        number += value;
+    }
+
+    const int getNumber() const { return number; }
+
+    int number = 0;
+};
+
+TEST(JobRunner, Constructor)
+{
+    gepard::JobRunner jobRunner0;
+    EXPECT_EQ(jobRunner0.workerCount(), 1u) << "";
+
+    gepard::JobRunner jobRunner1(2u);
+    EXPECT_EQ(jobRunner1.workerCount(), 2u) << "";
+
+    gepard::JobRunner jobRunner2(-2u);
+    EXPECT_EQ(jobRunner2.workerCount(), 1u) << "";
+}
+
+TEST(JobRunner, BoundFunc)
+{
+    TestClass test;
+    {
+        gepard::JobRunner js(2);
+
+        js.addJob(std::bind(&TestClass::incNumberAndWait, &test, 1));
+        js.addJob(std::bind(&TestClass::incNumberAndWait, &test, 10));
+        js.addJob(std::bind(&TestClass::incNumberAndWait, &test, 1));
+        js.addJob(std::bind(&TestClass::incNumberAndWait, &test, 1));
+    }
+
+    EXPECT_EQ(test.number, 4) << "";
+
+    {
+        gepard::JobRunner js(2);
+
+        js.addJob(std::bind(&TestClass::addValue, &test, 1));
+        js.addJob(std::bind(&TestClass::addValue, &test, 2));
+        js.addJob(std::bind(&TestClass::addValue, &test, 3));
+        js.addJob(std::bind(&TestClass::addValue, &test, 4));
+    }
+
+    EXPECT_EQ(test.number, 14) << "";
+}
+
+TEST(JobRunner, CallbackFunc)
+{
+    int value = 0;
+    {
+        gepard::JobRunner js(2);
+
+        js.addJob([&]{ value++; }, [&]{ value = 10; });
+    }
+
+    EXPECT_EQ(value, 10) << "";
+}
+
+} // anonymous namespace
+
+#endif // GEPARD_JOB_SCHEDULER_TESTS_H
